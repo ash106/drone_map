adedb0664f7efa9a6a7e8fa2626c430b4e55aac0
Alex Howington <alex.howington@gmail.com>
2014-02-09 23:29:08 -0700

    Get data from DroneStream API,parse it for lat and lng points and pass those to the index view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,13 @@
 class HomeController < ApplicationController
   def index
+    response = Unirest.get("http://api.dronestre.am/data")
+
+    if response.body["status"] == "OK"
+      strikes = response.body["strike"]
+      @strike_locs = []
+      for strike in strikes
+        @strike_locs.push({ number: strike["number"], lat: strike["lat"], lon: strike["lon"] })
+      end
+    end
   end
 end
