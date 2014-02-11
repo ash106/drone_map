e8d51629f79f24683ca29aac6092ee53be371510
Alex Howington <alex.howington@gmail.com>
2014-02-10 18:42:18 -0700

    Add basic info about what markers are, add total number of deaths to homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,16 @@ class HomeController < ApplicationController
     response = Unirest.get("http://api.dronestre.am/data")
 
     if response.body["status"] == "OK"
+      min_deaths = 0
+      max_deaths = 0
       strikes = response.body["strike"]
+      for strike in strikes
+        min_deaths += strike["deaths_min"].to_i
+        max_deaths += strike["deaths_max"].to_i
+      end
       gon.strikes = strikes
       @strikes = strikes
+      @deaths = "between #{min_deaths} and #{max_deaths}"
     end
   end
 
